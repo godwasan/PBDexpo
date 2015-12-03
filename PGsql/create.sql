c670aef9caa659337e15f98cf3d6fea8f86ba1ab
godwasan <godwasan8@hotmail.com>
2015-12-03 17:38:32 +0100

    PBD
    Proyecto Ejemplo PBD BLOBS

---
+++ PGsql/create.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE TABLE Peliculas
+(
+  foto oid,
+  codigo NUMERIC,
+  nombre VARCHAR(32)
+)
+WITH (
+  OIDS=FALSE
+)
+ALTER TABLE Peliculas
+  OWNER TO postgres;
---
+++ PGsql/funciones.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION get(pcodigo IN NUMERIC) RETURNS bytea AS $$
+DECLARE
+poid oid;
+BEGIN
+select foto into poid from peliculas where codigo = pcodigo;
+RAISE NOTICE '%', pcodigo;
+
+
+RETURN lo_get(oid);
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION set(pcodigo IN NUMERIC, pnombre IN VARCHAR , path IN VARCHAR ) RETURNS void AS $$
+DECLARE
+BEGIN
+insert into peliculas values (lo_import(path),pcodigo, pnombre); 
+
+END
+$$ LANGUAGE plpgsql;
+
